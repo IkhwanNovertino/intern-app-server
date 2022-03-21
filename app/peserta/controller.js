e390c8c016c4c427dd05bffaeeb81f4a5f727b73
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2022-03-21 11:22:18 +0800

    finish coding section peserta

--- app/peserta/controller.js
+++ app/peserta/controller.js
@@ -1,9 +1,106 @@
+const moment = require('moment');
+const Biro = require('../biro/model');
+const Pembimbing = require('../pembimbing/model');
+const Peserta = require('./model');
+
 module.exports = {
   index: async (req, res) => {
     try {
-      res.render('view_peserta', { title: "Ikhwan" })
+      const peserta = await Peserta.find()
+        .populate('biro');
+
+      let tglFormat = (value) => {
+        let tgl = moment(value).format("D MMM YYYY");
+        return tgl
+      }
+
+      res.render('admin/peserta/view_peserta', {
+        peserta,
+        tglFormat,
+      })
     } catch (err) {
       console.log(err);
+    }
+  },
+  viewCreate: async (req, res) => {
+    try {
+      const biro = await Biro.find();
+      const pembimbing = await Pembimbing.find();
+
+      res.render('admin/peserta/create', {
+        biro,
+        pembimbing
+      })
+    } catch (err) {
+      console.log(err);
+      res.redirect('/peserta');
+    }
+  },
+  actionCreate: async (req, res) => {
+    try {
+      const { name, nim, instansi, jurusan, email, tglmulai, tglselesai, pembimbing, biro } = req.body;
+
+      let peserta = await Peserta({ name, nim, instansi, jurusan, email, tglmulai, tglselesai, pembimbing, biro })
+      await peserta.save();
+
+      res.redirect('/peserta');
+    } catch (err) {
+      console.log(err);
+      res.redirect('/peserta')
+    }
+  },
+  viewEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const peserta = await Peserta.findById(id)
+      const biro = await Biro.find();
+      const pembimbing = await Pembimbing.find();
+
+      let tglFormat = (value) => {
+        let tgl = moment(value).format("L");
+        return tgl
+      }
+
+      res.render('admin/peserta/edit', {
+        pembimbing,
+        biro,
+        peserta,
+        tglFormat
+      });
+    } catch (err) {
+      console.log(err);
+      res.redirect('/peserta')
+    }
+  },
+  actionEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { name, nim, instansi, jurusan, email,
+        tglmulai, tglselesai, pembimbing, biro } = req.body;
+
+      // res.send(req.body)
+      await Peserta.findOneAndUpdate(
+        { _id: id },
+        {
+          name, nim, instansi, jurusan, email,
+          tglmulai, tglselesai, pembimbing, biro
+        })
+
+      res.redirect('/peserta');
+    } catch (err) {
+      console.log(err);
+      res.redirect('/peserta')
+    }
+  },
+  actionDelete: async (req, res) => {
+    try {
+      const { id } = req.params;
+      await Peserta.deleteOne({ _id: id });
+
+      res.redirect('/peserta')
+    } catch (err) {
+      console.log(err);
+      res.redirect('/peserta')
     }
   }
 }
---
+++ app/peserta/model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+let pesertaSchema = mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'nama harus diisi']
+  },
+  nim: {
+    type: String,
+    require: [true, 'no. induk harus diisi']
+  },
+  jurusan: {
+    type: String,
+    require: [true, 'jurusan harus diisi']
+  },
+  instansi: {
+    type: String,
+    require: [true, 'instansi/ asal sekolah harus diisi']
+  },
+  email: {
+    type: String,
+  },
+  tglmulai: {
+    type: Date,
+    default: Date.now()
+  },
+  tglselesai: {
+    type: Date,
+    default: Date.now()
+  },
+  pembimbing: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Pembimbing'
+  },
+  biro: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Biro'
+  }
+
+}, { timestamps: true })
+
+module.exports = mongoose.model('Peserta', pesertaSchema);
